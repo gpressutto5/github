7e2b18f51d160e49f9119dce3e2aba1283adb46e
Guilherme Pressutto <gpressutto5@gmail.com>
2018-01-24 00:50:15 -0200

    Ajax

--- src/components/search.js
+++ src/components/search.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import ajax from '@fdaciuk/ajax'
 
 const Search = () => {
   return (
     <div className='search'>
-      <input type='search' placeholder='Type the username' />
+      <input
+        type='search'
+        placeholder='Type the username'
+        onKeyUp={(e) => {
+          const key = e.which || e.keyCode
+          const ENTER = 13
+          if (key !== ENTER) {
+            return
+          }
+          ajax().get(`https://api.github.com/users/${e.target.value}`)
+          .then((result) => { console.log(result) })
+        }}
+      />
     </div>
   )
 }
