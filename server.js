73287b94854a540abfee74d06ac0db9be56d2fb7
Guilherme Pressutto <gpressutto5@gmail.com>
2018-01-23 23:36:43 -0200

    Initial Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  stats: { colors: true }
+}).listen('3000', (error) => {
+  if (error) {
+    return console.log(error);
+  }
+  console.log('Listening on http://localhost:3000');
+});
